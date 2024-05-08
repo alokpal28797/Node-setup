ffea7edcb290fdc731d6f047a9ad01ce5e3a7c55
alokpal28797 <alokpal28797@gmail.com>
2024-05-08 17:25:45 +0530

    controller added

--- app/utils/asyncHandler.js
+++ app/utils/asyncHandler.js
@@ -4,7 +4,7 @@
 
 export const asyncHandler = (fn) => async (req, res, next) => {
     try {
-
+        await fn(req, res, next);
     } catch (error) {
         res.status(error.code || 500).json({
             success: false,
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+import userRouter from "./app/routes/user.routes.js";
 
 dotenv.config({
     path: './.env'
@@ -20,6 +21,10 @@ app.use(express.json({ limit: '16kb' })) // to parse body data
 app.use(express.urlencoded({ extended: true, limit: '16kb' })) // to parse data pased from url
 app.use(express.static("public")) //  to store file folders
 app.use(cookieParser());  // to store cookies from server to browser securely
+
+// routes import
+
+app.use('/api/v1/users',userRouter)
 
 connectDb()
     .then(() => {
